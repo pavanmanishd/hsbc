7b1a80128d2773c94719e2cf4c387f26951e862c
pavanmanishd <pavanmanishd@gmail.com>
2024-08-21 11:15:25 +0530

    jwt authentication

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const connectDB = require('./config/db.js');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// Connect to the database
+connectDB();
+
+const app = express();
+
+// Init Middleware
+app.use(express.json());
+
+app.use('/api/auth', require('./routes/auth'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
